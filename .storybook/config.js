d0cccbc718e121a032c275f04ea374236296bbcd
vomvoru <qkrcjfgus3@gmail.com>
2019-07-13 17:23:46 +0900

    stories 파일 한꺼번에 불러오기

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,8 @@ import { configure } from '@storybook/react';
 import '../src/common';
 
 function loadStories() {
-  require('../stories/Header.stories');
-  require('../stories/Main.stories');
-  require('../stories/Footer.stories');
-  // You can require as many stories as you need.
+  const req = require.context('../stories', true, /^((?![\\/]node_modules|vendor[\\/]).)*\.stories\.[jt]sx?$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
