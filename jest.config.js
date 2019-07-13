72f35a1d5964f3bda9f29cc7b2d4e6a18bc62532
Park Cheol Hyeon <pch33@tmon.co.kr>
2019-07-13 17:05:01 +0900

    jest 에서 이미지나 스타일 파일 import하지 못하는 이슈 해결
    mock 파일을 import 하는 방식으로 해결

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
 
+const ROOT = __dirname;
+
 module.exports = {
   transform: {
     '^.+\\.jsx?$': path.resolve(__dirname, './jest.config.babel.js'),
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
+      ROOT,
+      './__mocks__/fileMock.js'
+    ),
+    '\\.(css|less)$': path.resolve(ROOT, './__mocks__/fileMock.js'),
+  },
 };
