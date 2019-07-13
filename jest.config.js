6f1e7a08d9737353b9d1b2b6afe2852a8c975b20
Park Cheol Hyeon <pch33@tmon.co.kr>
2019-07-14 02:10:01 +0900

    jest.config.js 코드 정리

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 const ROOT = __dirname;
 
+const mockModulePath = path.resolve(ROOT, './__mocks__/fileMock.js');
+const imageFileRegex =
+  '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$';
+const cssFileRegex = '\\.(css|scss)$';
+
 module.exports = {
   transform: {
     '^.+\\.jsx?$': path.resolve(__dirname, './jest.config.babel.js'),
@@ -9,10 +14,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
   setupFilesAfterEnv: [path.resolve(ROOT, './jest.setup.js')],
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
-      ROOT,
-      './__mocks__/fileMock.js'
-    ),
-    '\\.(css|less)$': path.resolve(ROOT, './__mocks__/fileMock.js'),
+    [imageFileRegex]: mockModulePath,
+    [cssFileRegex]: mockModulePath,
   },
 };
