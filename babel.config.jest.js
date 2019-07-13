4ff20ffafd62d12e2b8fd4e997c1088a1a64adc6
vomvoru <qkrcjfgus3@gmail.com>
2019-07-13 18:44:34 +0900

    필요없는 jest babel 설정 삭제

--- babel.config.jest.js
+++ babel.config.jest.js
@@ -6,14 +6,5 @@ const babelConfig = require('./babel.config');
 const webpackConfigPath = path.resolve(__dirname, './webpack.config.common.js');
 
 module.exports = mrege(babelConfig, {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: false,
-        modules: 'cjs',
-      },
-    ],
-  ],
   plugins: ['require-context-hook', ['babel-plugin-webpack-alias', { config: webpackConfigPath }]],
 });
