b8e9abaf220abcf8d0d2e092c68b992bc4746ffd
vomvoru <qkrcjfgus3@gmail.com>
2019-07-14 03:17:31 +0900

    모바일 화면에서 스크린샷 테스트를 하도록 설정 수정

--- test/storyshots.test.js
+++ test/storyshots.test.js
@@ -1,6 +1,12 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
+const devices = require('puppeteer/DeviceDescriptors');
+
+const iPhone = devices['iPhone 6'];
+
+const customizePage = page => page.emulate(iPhone);
+
 const getScreenshotOptions = () => ({
   fullPage: true,
 });
@@ -17,6 +23,7 @@ initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
     storybookUrl: 'http://localhost:6006',
+    customizePage,
     getScreenshotOptions,
     getGotoOptions,
   }),
