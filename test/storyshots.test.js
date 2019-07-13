ba5643da6eacd96fafdbd92e4e435ef4cc1297af
vomvoru <qkrcjfgus3@gmail.com>
2019-07-14 03:14:52 +0900

    화면 전체에 대한 스크린샷  테스트를 진행하도록 설정 수정

--- test/storyshots.test.js
+++ test/storyshots.test.js
@@ -1,6 +1,18 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
+const getScreenshotOptions = () => ({
+  fullPage: true,
+});
+
+// React renederer 테스트
 initStoryshots();
 
-initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() });
+// screenshot image 테스트
+initStoryshots({
+  suite: 'Image storyshots',
+  test: imageSnapshot({
+    storybookUrl: 'http://localhost:6006',
+    getScreenshotOptions,
+  }),
+});
