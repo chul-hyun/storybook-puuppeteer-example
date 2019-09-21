462e395d974e42f1187faf996238370bdbc374a2
vomvoru <qkrcjfgus3@gmail.com>
2019-09-22 03:02:50 +0900

    설정파일 수정

--- github-bot-madgerc.js
+++ github-bot-madgerc.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  webpackConfig: path.resolve('../webpack.config.resolve.js'),
+  src: path.resolve('./src/index.jsx'),
+  webpackConfig: path.resolve('./webpack.config.resolve.js'),
 };
